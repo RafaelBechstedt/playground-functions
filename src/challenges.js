cb20089f456ddae3e3ee72b7af4673dc69a0cbc1
Rafael <bechstedt@hotmail.com>
2022-05-25 13:36:28 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -82,11 +82,24 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+  let fraseCodificada = frase
+    .replace(/a/g, 1)
+    .replace(/e/g, 2)
+    .replace(/i/g, 3)
+    .replace(/o/g, 4)
+    .replace(/u/g, 5)
+  return fraseCodificada
 }
-function decode() {
-  // seu código aqui
+
+function decode(frase) {
+  let fraseDecodificada = frase
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u')
+  return fraseDecodificada
 }
 
 // Desafio 10
